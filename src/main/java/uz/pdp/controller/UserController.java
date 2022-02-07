80022c5edfee2fbb2828c7a10a8bdd1d51f55ccf
User <zoyirovj@mail.ru>
2022-02-08 02:33:07 +0500

    doget/dopost

--- src/main/java/uz/pdp/controller/UserController.java
+++ src/main/java/uz/pdp/controller/UserController.java
@@ -1,11 +1,27 @@
 package uz.pdp.controller;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet("/api/user")
 public class UserController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+
+
+    }
 }
 
 
