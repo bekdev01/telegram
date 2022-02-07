a1b26a07e16865caa8168e8b783462e23909b180
Khusan <erkinovhusan1@gmail.com>
2022-02-07 21:48:19 +0500

    added codes

--- src/main/java/uz/pdp/controller/UserController.java
+++ src/main/java/uz/pdp/controller/UserController.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 public class UserController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
     }
 
     @Override
