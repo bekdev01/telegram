cc6e19c75e9dededfc96aa7905e5ea91a7cc0569
davronismoilov <ismoilovdavron16@gmail.com>
2022-02-08 09:55:51 +0500

    database

--- src/main/java/database/user/UserDatabase.java
+++ src/main/java/database/user/UserDatabase.java
@@ -1,14 +1,14 @@
 package database.user;
 
-import com.mongodb.client.MongoClient;
+
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.Updates;
-import com.mongodb.client.result.DeleteResult;
+
 import database.base.BaseDatabase;
 import database.utils.MongoUtils;
-import model.Message;
+
 import model.User;
 import org.bson.Document;
 import org.bson.conversions.Bson;
@@ -16,13 +16,12 @@ import org.bson.types.ObjectId;
 
 
 import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.Date;
 
 
 import static com.mongodb.client.model.Filters.and;
 import static com.mongodb.client.model.Filters.eq;
-
 
 public class UserDatabase implements BaseDatabase {
 
@@ -48,45 +47,28 @@ public class UserDatabase implements BaseDatabase {
     }
 
 
-    public boolean addMessage(String toId, String fromId, String text) {
+    public boolean addMessage(String fromId, String toId, String text) {
         MongoDatabase connection = getDatabase();
         MongoCollection<Document> users = connection.getCollection("users");
 
         Document fromUser = getUser(fromId);
         Document toUser = getUser(toId);
 
-        Bson filterFrom = and(eq("_id", new ObjectId(fromId)), eq("messages.id", toId));
-        Bson filterTo = and(eq("_id", new ObjectId(toId)), eq("messages.id", fromId));
+        Bson filterFrom = and(eq("_id", new ObjectId(fromId)),
+                eq("messages.id", toId));
+        Bson filterTo = and(eq("_id", new ObjectId(toId)),
+                eq("messages.id", fromId));
 
         Document docFrom = users.find(filterFrom).first();
         Document docTo = users.find(filterTo).first();
         if (docFrom == null && docTo == null) {
-            Document message = new Document();
-            message.append("text", text)
-                    .append("date", new Date())
-                    .append("isMine", true);
-            ArrayList<Document> msg = new ArrayList<>();
-            msg.add(message);
-            Document document = new Document()
-                    .append("id", toId)
-                    .append("messageUser", msg);
-
-
-            Bson updates = Updates.push("messages", document);
+            Bson updates = MongoUtils.pushMessage(text, toId, true);
             users.updateOne(fromUser, updates);
 
-            message.put("isMine", false);
-            msg = new ArrayList<>();
-            msg.add(message);
-            document = new Document()
-                    .append("id", fromId)
-                    .append("messageUser", msg);
-
-            updates = Updates.push("messages", document);
+            updates = MongoUtils.pushMessage(text, fromId, false);
             users.updateOne(toUser, updates);
 
             return true;
-
         }
 
         Document document = new Document();
@@ -106,88 +88,17 @@ public class UserDatabase implements BaseDatabase {
 
     public ArrayList<Document> getMessage(String fromId, String toId) {
 
-        MongoCollection<Document> users = getDatabase().getCollection("users");
         Document user = getUser(fromId);
         ArrayList<Document> messages = (ArrayList<Document>) user.get("messages");
+
         for (var msg : messages) {
             if (msg.get("id").equals(toId)) {
-                return  (ArrayList<Document>) msg.get("messageUser");
+                return (ArrayList<Document>) msg.get("messageUser");
 
             }
         }
-
-
         return null;
     }
-
-
-//
-//    @Override
-//    public Document getObject(String id) {
-//        MongoDatabase database = getDatabase();
-//        Document user = database.getCollection("users").find(Filters.eq("_id", new ObjectId(id))).first();
-//
-//        return user;
-//
-//    }
-//
-//    @Override
-//    public boolean deleteObject(String id) {
-//
-//        MongoDatabase database = getDatabase();
-//        MongoCollection<Document> users = database.getCollection("users");
-//        DeleteResult id1 = users.deleteOne(Filters.eq("_id", new ObjectId(id)));
-//        System.out.println(id1);
-//
-//
-//        return false;
-//    }
-//
-//    @Override
-//    public List<Document> getObjectList() {
-//        List<Document> userList = new ArrayList<>();
-//        MongoDatabase tm = getDatabase();
-//        MongoCollection<Document> users =
-//                tm.getCollection("users");
-//        for (var doc:users.find()) {
-//            userList.add(doc);
-//        }
-//
-//        return userList;
-//    }
-//
-//    public boolean addTaskToUser(Document task, Document user) {
-//        MongoDatabase connection = getDatabase();
-//        MongoCollection<Document> users = connection.getCollection("users");
-//        Bson updates = Updates.combine(Updates.addToSet("tasks", task));
-//        users.updateOne(user, updates);
-//
-//        return true;
-//    }
-
-
-//
-//    public static void main(String[] args) {
-//        UserDatabase userDatabase = new UserDatabase();
-////        User user = new User("mmmm" ,"asa" , "asads ," ,
-////                null);
-////       userDatabase.addObject(user);
-//        MongoDatabase database = userDatabase.getDatabase();
-//        MongoCollection<Document> users = database.getCollection("users");
-//        for (var doc: users.find()) {
-//            System.out.println(doc.get("_id"));
-//        }
-//
-//
-//    }
-
-//    public static void main(String[] args) {
-//        UserDatabase userDatabase = new UserDatabase();
-//        ArrayList<Document> message = userDatabase.getMessage("62014ae137b08d3d00883e32", "6201ea601781a52c21662c41");
-////        userDatabase.addUser(new User("forbid","accept", "party","drive"));
-//        System.out.println(message);
-////        userDatabase.addMessage("6201ea601781a52c21662c41", "62014ae137b08d3d00883e32" , "sd");
-//    }
 
 
 }
--- src/main/java/database/utils/MongoUtils.java
+++ src/main/java/database/utils/MongoUtils.java
@@ -1,9 +1,12 @@
 package database.utils;
 
+import com.mongodb.client.model.Updates;
 import model.User;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 
 public class MongoUtils {
@@ -15,10 +18,22 @@ public class MongoUtils {
                 .append("lastname", user.getLastName())
                 .append("email", user.getEmail())
                 .append("messages", messages);
-
-
-
         return doc;
+    }
+
+    public static Bson pushMessage(String text , String id  , boolean isMine){
+        Document message = new Document();
+        message.append("text", text)
+                .append("date", new Date())
+                .append("isMine", isMine);
+        ArrayList<Document> msg = new ArrayList<>();
+        msg.add(message);
+        Document document = new Document()
+                .append("id", id)
+                .append("messageUser", msg);
+
+
+        return Updates.push("messages", document);
     }
 
 
